fa600e17252344d42b7a7604089b0c0919607707
unknown <mika.2405.yy@gmail.com>
2025-06-26 15:56:58 +0900

    DBに登録されているデータを取得しました

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo; //追加
+
 class TodoController extends Controller
 {
      public function index()
     {
+        $todo = new Todo(); //追加
+        $todos = $todo->all(); //追加
+
         return view('todo.index'); // 修正
     }
 }
+
+
