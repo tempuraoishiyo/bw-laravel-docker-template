8c897c4a89552f10c987edf8fc181060a6976d85
unknown <mika.2405.yy@gmail.com>
2025-06-27 16:56:56 +0900

    入力したToDoの内容を送信した際にDBに新規作成されるように登録処理を実装する

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -16,17 +16,30 @@ class TodoController extends Controller
         return view('todo.index',['todos' => $todos]); // 修正
     }
 
-    //public function create()
-//  {
+    //public function store()
+  //{
     //dd('新規作成画面のルート実行！');
 
     
-//   }
+   //}
    public function create()
 {
     $todos = Todo::all();
     return view('todo.create', ['todos' => $todos]);
 }
+   public function store(Request $request)
+   {
+     $content = $request->input('content');
+
+     // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+    $todo = new Todo(); 
+    // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+    $todo->content = $content;
+    // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+    $todo->save();
+    
+    return redirect()->route('todo.index'); // 追記
+   }
 
 }
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -13,5 +13,7 @@
 
 Route::get('/todo','TodoController@index');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create'); // 追記
-    //return view('welcome');
+Route::post('/todo','TodoController@store')->name('todo.store');
+Route::get('/todo', 'TodoController@index')->name('todo.index'); // ルート名の定義を追記
+//return view('welcome');
     
